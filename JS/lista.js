49eb7314110bb85b9df5417bc23220d4eb00ad62
javaswingdev <javaswingdev@gmail.com>
2025-04-05 01:40:53 +0000

    Cambios2

--- JS/lista.js
+++ JS/lista.js
@@ -1,12 +1,14 @@
-function mostrarListas(listapokes){
+function mostrarlista(listapokes) {
     const app = document.getElementById("app");
     let Pokes = "";
 
-    for (let i = 0; i <listapokes.length; i++>
-        led id = listapokes[i].url.split("/")[6];
-        Pokes 
-    
-    
-
-
-}
+    for (let i = 0; i < listapokes.length; i++) {
+        let id = listapokes[i].url.split("/")[6];
+        Pokes += `
+        <div class="c-lista-pokemon poke-${id}" onclick="mostrarDetalle('${listapokes[i].name}')">
+            <p>#${id}</p>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" width="auto" height="60" loading="lazy" alt="${listapokes[i].name}">
+            <p>${listapokes[i].name}</p>
+        </div>`;
+    }
+    app.innerHTML = Pokes;
