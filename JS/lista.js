a112be7f6c65730cc38ac9f8de9db7b69339b644
javaswingdev <javaswingdev@gmail.com>
2025-04-05 01:33:35 +0000

    Cambios

---
+++ JS/lista.js
@@ -0,0 +1,12 @@
+function mostrarListas(listapokes){
+    const app = document.getElementById("app");
+    let Pokes = "";
+
+    for (let i = 0; i <listapokes.length; i++>
+        led id = listapokes[i].url.split("/")[6];
+        Pokes 
+    
+    
+
+
+}
